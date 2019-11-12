d54aeda1e2ca3d730583eaa9937029c8b036d905
Douglas Lima <douglas.felipelima18@gmail.com>
2019-11-12 10:41:35 -0300

    . ala ranna

--- include/bTree.h
+++ include/bTree.h
@@ -1,21 +1,24 @@
 #ifndef BTREE
 #define BTREE
+#include <forward_list> 
 
 struct Node {
 	unsigned int order;
-	int* data;
-	Node* filhos; 
-
+	std::forward_list<int> data;
+	std::vector<Node*>filhos;
 };
 
 
-bool search (Node* tree, T key){
-	
-	for(int cont = 0; cont < tree->order; ++cont){
-		if ( tree->data[cont] == key)
+bool search (Node* tree, int key){
+	int cont = 0;
+	auto it = data.begin();
+	while(it != data.end()){
+		if ( *it == key)
 			return true;
-		if ( tree->data[cont] > key)
+		if ( *it > key)
 			return search(tree->filhos[cont], data);
+		cont++;
+		it++;
 	}
 	return false;
 };
@@ -24,11 +27,11 @@ bool search (Node* tree, T key){
 bool insert(Node* tree, T data){
 
 	return false;
-}
+};
 
 
 bool remove (Node* tree, T data){
 
-	return false
-}
+	return false;
+};
 #endif
