510cbc77a5cffed2f842ef733024b25ec65b4fa8
rannaraabe <rannaraabe@hotmail.com>
2019-11-15 18:07:54 -0300

    ajeitando tudo

--- include/bTree.hpp
+++ include/bTree.hpp
@@ -15,106 +15,22 @@ using namespace std;
 struct Node {
 	std::vector<int> data;			// Valores dos nós
 	std::vector<Node*> filhos;		// Ponteiros de cada nós
-	int order;
-	bool folha;
+	int order;						// Ordem da pag
+	bool folha;						// Bool para saber se a pagina eh folha
 	// Construtor do node
-    Node() : data(), filhos() {}
 	Node(int _order, bool _folha) : order{_order}, folha{_folha}
 	{}
 };
 
-
-void cisao(Node* s, int i, Node *y);
-
+bool search(Node* tree, int key);
+void insert(Node* root, int k);
 void insertNonFull(Node* s, int k);
+void cisao(Node* s, int i, Node *y);
+void ordenar(vector<int> vector);
+void remove(Node* tree, int key);
+void concatenacao(Node* tree, int index_pag);
+void redistribuicao(Node* tree, int index_pag);
 
-void insert(Node* root, int k) 
-{ 
-    
-    if (root == NULL) 
-    { 
-        root = new Node(root->order, true); 
-        root->data.push_back(k);  
-		root->folha = true;
-    } 
-    else
-    { 
-        if (root->data.size() == 2*root->order-1) 
-        { 
-            Node *s = new Node(root->order, false); 
-  
-            s->filhos.push_back(root); 
-  
-            cisao(s, 0, root); 
-  
-            int i = 0; 
-            if (s->data[0] < k) 
-                i++; 
-            insertNonFull(s->filhos[i],k); 
-  
-            root = s; 
-        } 
-        else 
-            insertNonFull(root, k); 
-    } 
-} 
-  
-void insertNonFull(Node* s, int k) 
-{ 
-    int i = s->data.size() - 1; 
-  
-    if (s->folha == true) 
-    { 
-        while (i >= 0 && s->data[i] > k) 
-        { 
-            s->data[i+1] = s->data[i]; 
-            i--; 
-        } 
-  
-        s->data.insert(s->data.begin() + i + 1, k);
-    } 
-    else 
-    { 
-        
-        while (i >= 0 && s->data[i] > k) 
-            i--; 
-  
-        if (s->filhos[i+1]->data.size() == 2*s->order-1) 
-        { 
-            cisao(s, i+1, s->filhos[i+1]); 
-  
-            if (s->data[i+1] < k) 
-                i++; 
-        } 
-        insertNonFull(s->filhos[i+1], k); 
-    } 
-} 
-  
-void cisao(Node* s, int i, Node *y) 
-{ 
-    Node *z = new Node(y->order, y->folha); 
-    for (int j = 0; j < y->order-1; j++) {
-        z->data.push_back(y->data[j+y->order]); 
-	}
-	y->data.erase(y->data.begin() + y->order,y->data.end());
-  
-    if (y->folha == false) 
-    { 
-        for (int j = 0; j < s->order; j++) 
-            z->filhos.push_back(y->filhos[j+y->order]); 
-    } 
-  
-  
-    for (int j = s->data.size(); j >= i+1; j--) 
-        s->filhos[j+1] = s->filhos[j]; 
-  
-    s->filhos[i+1] = z; 
-  
-    for (int j = s->data.size()-1; j >= i; j--) 
-        s->data[j+1] = s->data[j]; 
-  
-    s->data[i] = y->data[s->order-1]; 
-} 
 bool search(Node* tree, int key){
 	if(tree == nullptr)
 		return false;
@@ -136,6 +52,79 @@ bool search(Node* tree, int key){
     
 	return search(tree->filhos[i], key); 
 }
+
+void insert(Node* root, int k) { 
+    if(root == NULL) { 
+        root = new Node(root->order, true); 
+        root->data.push_back(k);  
+		root->folha = true;
+    } 
+    else { 
+        if(root->data.size() == 2*root->order-1){ 
+            Node *s = new Node(root->order, false); 
+  
+            s->filhos.push_back(root); 
+  
+            cisao(s, 0, root); 
+  
+            int i = 0; 
+            if (s->data[0] < k) 
+                i++; 
+          
+		    insertNonFull(s->filhos[i],k); 
+            root = s; 
+        } else 
+            insertNonFull(root, k); 
+    } 
+} 
+  
+void insertNonFull(Node* s, int k){ 
+    int i = s->data.size() - 1; 
+  
+    if(s->folha == true){ 
+        while(i >= 0 && s->data[i] > k){ 
+            s->data[i+1] = s->data[i]; 
+            i--; 
+        } 
+  
+        s->data.insert(s->data.begin() + i + 1, k);
+    } else { 
+        
+        while(i >= 0 && s->data[i] > k) 
+            i--; 
+  
+        if(s->filhos[i+1]->data.size() == 2*s->order-1){ 
+            cisao(s, i+1, s->filhos[i+1]); 
+  
+            if(s->data[i+1] < k) 
+                i++; 
+        } 
+        insertNonFull(s->filhos[i+1], k); 
+    } 
+} 
+  
+void cisao(Node* s, int i, Node *y){ 
+    Node *z = new Node(y->order, y->folha); 
+    for (int j = 0; j < y->order-1; j++) {
+        z->data.push_back(y->data[j+y->order]); 
+	}
+	y->data.erase(y->data.begin() + y->order,y->data.end());
+  
+    if (y->folha == false){ 
+        for (int j = 0; j < s->order; j++) 
+            z->filhos.push_back(y->filhos[j+y->order]); 
+    } 
+  
+    for (int j = s->data.size(); j >= i+1; j--) 
+        s->filhos[j+1] = s->filhos[j]; 
+  
+    s->filhos[i+1] = z; 
+  
+    for (int j = s->data.size()-1; j >= i; j--) 
+        s->data[j+1] = s->data[j]; 
+  
+    s->data[i] = y->data[s->order-1]; 
+} 
 
 void ordenar(vector<int> vector){
 	for(int i = 0; i<vector.size(); ++i)
@@ -204,8 +193,6 @@ void redistribuicao(Node* tree, int index_pag){		// nao eh propagavel
 	}
 
 	ordenar(pag_filha->data); // Ordeno pq eh provavel q ele insira valores nos lugares errados
-	cisao(pag_filha, pag_vizinha);
+	cisao(pag_filha, 0, pag_vizinha);
 }
-
 #endif
-v
